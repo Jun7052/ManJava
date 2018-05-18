1f4aad7c389e0449ef4585988e49a8a4b8287026
smhrc18 <smhrc18@DESKTOP-DG65JAN>
2018-05-18 11:24:58 +0900

    soju java club

--- java20180518/src/java20180518/Main.java
+++ java20180518/src/java20180518/Main.java
@@ -8,14 +8,13 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ArrayList<Person> arr = new ArrayList<>();
-		boolean btn = true;
-		
-		System.out.print("<<Soja Club>>");
-		
-		while(btn) {
+
+		System.out.print("<<Soja Club>>"); // Soju Java Club
+
+		while (true) {
 			System.out.print("[1]가입 [2]수정 [3]목록 [4]종료 >>");
 			int choice = sc.nextInt();
-			
+
 			switch (choice) {
 			case 1:
 				System.out.println("==회원가입==");
@@ -23,27 +22,23 @@ public class Main {
 				String input_name = sc.next();
 				System.out.print("주량 : ");
 				int input_soju = sc.nextInt();
-				System.out.println(input_name+"님이 추가되었습니다.");
-				System.out.println();
-				
+				System.out.println(input_name + "님이 추가되었습니다.");
+
 				Person pr = new Person(input_name, input_soju);
 				arr.add(pr);
 				break;
 			case 2:
-				
+
 				break;
 			case 3:
 				break;
-			case 4 :
-				btn = false;
+			case 4:
 				break;
 
 			default:
 				break;
 			}
 		}
-		
-
 
 	}
 
