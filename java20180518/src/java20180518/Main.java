e71d91ab1fef724ffd6a3a8fc0954f5fcf807022
smhrc23 <smhrc23@DESKTOP-PJS8TIJ>
2018-05-18 11:07:29 +0900

    새로운 기능 추가.

--- java20180518/src/java20180518/Main.java
+++ java20180518/src/java20180518/Main.java
@@ -1,15 +1,46 @@
 package java20180518;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		System.out.println("안녕하세요");
 		Scanner sc = new Scanner(System.in);
+		ArrayList<Person> arr = new ArrayList<>();
 		
-		System.out.print("점심 먹고싶다 나도 점심먹고싶다");
+		System.out.print("<<Soja Club>>");
+		
+		
+		while(true) {
+			System.out.print("[1]가입 [2]수정 [3]목록 [4]종료 >>");
+			int choice = sc.nextInt();
+			
+			switch (choice) {
+			case 1:
+				System.out.println("==회원가입==");
+				System.out.print("이름 : ");
+				String input_name = sc.next();
+				System.out.print("주량 : ");
+				int input_soju = sc.nextInt();
+				System.out.println(input_name+"님이 추가되었습니다.");
+				
+				Person pr = new Person(input_name, input_soju);
+				arr.add(pr);
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				break;
+			case 4 :
+				break;
+
+			default:
+				break;
+			}
+		}
+		
 
 
 	}
---
+++ java20180518/src/java20180518/Person.java
@@ -0,0 +1,29 @@
+package java20180518;
+
+public class Person {
+	String name;
+	int soju;
+	
+	public Person(String name, int soju) {
+		super();
+		this.name = name;
+		this.soju = soju;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSoju() {
+		return soju;
+	}
+
+	public void setSoju(int soju) {
+		this.soju = soju;
+	}
+	
+}
